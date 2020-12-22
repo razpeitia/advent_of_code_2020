801d8ed0792e4971445657814855425e208c4a1b
Ricardo Azpeitia Pimentel <ricardo.azpeitia@nextroll.com>
2020-12-21 18:40:44 -0600

    Day 21 solved

--- Day21/src/main.rs
+++ Day21/src/main.rs
@@ -1,16 +1,13 @@
 use std::fs;
 use std::io;
-use std::collections::HashMap;
-
-fn remove_ingredient(table : &mut HashMap<&str, HashMap<&str, u32>>, ingredient : &str) {
-    for (_k, v) in table.iter_mut() {
-        v.remove(ingredient);
-    }
-}
+use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
+use std::time::Instant;
 
 fn main() -> Result<(), io::Error> {
+    let now = Instant::now();
     let data = fs::read_to_string("assets/input.txt")?;
-    let mut table: HashMap<&str, HashMap<&str, u32>> = HashMap::new();
+    let mut table: HashMap<&str, HashSet<&str>> = HashMap::new();
     let mut ingredient_map : HashMap<&str, u32> = HashMap::new();
     for line in data.lines() {
         let parts : Vec<&str> = line.split(" (contains ").collect();
@@ -19,48 +16,48 @@ fn main() -> Result<(), io::Error> {
         for &ingredient in ingredients.iter() {
             *ingredient_map.entry(ingredient).or_insert(0) += 1;
         }
+        let set_ingredients : HashSet<&str> = HashSet::from_iter(ingredients.iter().cloned());
         for &allergen in allergens.iter() {
-            for &ingredient in ingredients.iter() {
-                *table.entry(allergen).or_insert(HashMap::new()).entry(ingredient).or_insert(0) += 1;
+            match table.get_mut(allergen) {
+                Some(v) => {
+                    let intersection : Vec<&str> = v.intersection(&set_ingredients).map(|&x| x).collect();
+                    v.clear();
+                    v.extend(intersection.iter().cloned());
+                }
+                None => {
+                    table.insert(allergen, set_ingredients.clone());
+                }
             }
         }
     }
+    println!("Read input took {:?}", now.elapsed());
 
     // Part 1
-    println!("{:?}", table);
-
-
+    let now = Instant::now();
     let mut allergen_map : HashMap<&str, &str> = HashMap::new();
-    let allergens : Vec<&str> = table.keys().map(|&x| x).collect();
+    let mut taken : HashSet<&str> = HashSet::new();
+    let mut all_allergens : Vec<&str> = table.keys().map(|&x| x).collect();
     while allergen_map.len() != table.len() {
-        for &allergen in allergens.iter() {
-            let v = table.get_mut(allergen).unwrap();
-            if v.len() == 0 { continue; }
+        for &allergen in all_allergens.iter() {
+            let v : Vec<&str> = table.get(allergen).unwrap().difference(&taken).map(|&x| x).collect();
             if v.len() == 1 {
-                let ingredients : Vec<&str> = v.keys().map(|&x| x).collect();
-                let &ingredient = ingredients.first().unwrap();
-                allergen_map.insert(allergen, ingredient);
-                remove_ingredient(&mut table, ingredient);
-            } else {
-                let (&ingredient, &count) = v.iter().max_by_key(|(_ingredient, &count)| count).unwrap();
-                if count == 1 { continue; }
-                allergen_map.insert(allergen, ingredient);
-                remove_ingredient(&mut table, ingredient);
+                for &ingredient in v.iter() {
+                    allergen_map.insert(allergen, ingredient);
+                    taken.insert(ingredient);
+                }
             }
         }
     }
-    for (_, &ingredient) in allergen_map.iter() {
-        ingredient_map.remove(ingredient).unwrap();
-    }
-    let ans : u32 = ingredient_map.iter().map(|(_, &v)| v).sum();
-    println!("{:}", ans);
 
-    let mut allergens : Vec<&str> = allergen_map.keys().map(|&x| x).collect();
-    allergens.sort();
-    for &allergen in allergens.iter() {
-        let &ingredient = allergen_map.get(allergen).unwrap();
-        println!("{} {}", allergen, ingredient);
-    }
+    let ans : u32 = ingredient_map.iter().filter(|(&ingredient, _)| !taken.contains(ingredient)).map(|(_, &v)| v).sum();
+    println!("{}", ans);
+    println!("Part 1     took {:?}", now.elapsed());
 
+    // Part 2
+    let now = Instant::now();
+    all_allergens.sort();
+    let bad_ingredients : Vec<&str> = all_allergens.iter().map(|&x| *allergen_map.get(x).unwrap()).collect();
+    println!("{}", bad_ingredients.join(","));
+    println!("Part 2     took {:?}", now.elapsed());
     Ok(())
 }
